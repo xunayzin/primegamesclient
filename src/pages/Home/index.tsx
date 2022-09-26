325a66d5ebfe4123f9a3601cb07f9552a76c6ed0
Your Name <mxunay@gmail.com>
2022-09-26 17:57:43 -0300

    chore: update `home style and index.tsx`

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,40 @@
 import * as S from "./style";
+import { ReactComponent as Search } from "assets/icons/search.svg";
 
 const Home = () => {
-	return <S.Home></S.Home>;
+  return (
+    <S.Home>
+      <S.HomeContent>
+        <header>
+          <S.HomeHeaderDetails>
+            <div>
+              <S.HomeHeaderDetailsLogo>
+                Prime Games from Xbox{" "}
+              </S.HomeHeaderDetailsLogo>
+              <S.HomeHeaderDetailsDate>
+                Aqui ficará a data
+              </S.HomeHeaderDetailsDate>
+            </div>
+            <S.HomeHeaderDetailsSearch>
+              <Search />
+              <input type="text" placeholder="Pesquisa" />
+            </S.HomeHeaderDetailsSearch>
+          </S.HomeHeaderDetails>
+        </header>
+        <div>
+          <S.HomeGameTitle>
+            <b>Jogos</b>
+          </S.HomeGameTitle>
+          <S.HomeGameList>
+            <p>Lista de jogos</p>
+          </S.HomeGameList>
+        </div>
+      </S.HomeContent>
+      <aside>
+        <p>Favoritos</p>
+      </aside>
+    </S.Home>
+  );
 };
 
 export default Home;
