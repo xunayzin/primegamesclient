ad9b7984687fed503f0df9e3cbdee00986e95e1f
Your Name <mxunay@gmail.com>
2022-09-26 17:12:51 -0300

    chose: install `react-router-dom` ; feat(app): implement `ThemeProvider, BrowserRouter, Router` ; chose: create `router.tsx` ; chose: create `routes.ts` ; chose: install `react-query`

---
+++ src/types/routes.ts
@@ -0,0 +1,9 @@
+export enum RoutePath {
+    HOME = '/',
+    LOGIN = '/login',
+    SETTINGS = '/settings',
+    SETTINGS_PRODUCTS = 'game',
+    SETTINGS_TABLES = 'genre',
+    SETTINGS_USERS = 'user',
+}
+  
