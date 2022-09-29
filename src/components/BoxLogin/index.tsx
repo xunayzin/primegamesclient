73990a5393f5d646c359ee67ab8ef1513b7ddf96
Your Name <mxunay@gmail.com>
2022-09-29 19:13:49 -0300

    doc(ButtonLarge): created `login button`

--- src/components/BoxLogin/index.tsx
+++ src/components/BoxLogin/index.tsx
@@ -1,5 +1,6 @@
 import * as S from "./style";
 import logo from "assets/imgs/logo.png";
+import ButtonLarge from "components/ButtonLarge";
 
 const BoxLogin = () => {
   return (
@@ -14,6 +15,7 @@ const BoxLogin = () => {
       <S.BoxLoginForm>
         <input type="text" placeholder="E-mail" />
         <input type="password" placeholder="Senha" />
+        <ButtonLarge value="Entrar" type="button" />
       </S.BoxLoginForm>
     </S.BoxLogin>
   );
---
+++ src/components/ButtonLarge/style.ts
@@ -0,0 +1,24 @@
+import styled, { css } from "styled-components";
+
+export const ButtonLarge = styled.button`
+	${({ theme }) => css`
+		background: ${theme.colors.primaryColor};
+		border: none;
+		border-radius: 8px;
+		color: ${theme.colors.textColor};
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 48px;
+		width: 100%;
+		cursor: pointer;
+		${theme.mixins.bodyStyle()};
+		font-weight: 600;
+		&:disabled {
+			color: ${theme.colors.textDark};
+			background: ${theme.colors.baseBg1};
+			box-shadow: none;
+			cursor: not-allowed;
+		}
+	`}
+`;
