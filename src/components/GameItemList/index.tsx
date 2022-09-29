c510adc6f9b385f904d99129396e99b265e6c4b3
Your Name <mxunay@gmail.com>
2022-09-29 19:35:27 -0300

    fix(GameItemList): correcting index.tsx

--- src/components/GameItemList/index.tsx
+++ src/components/GameItemList/index.tsx
@@ -1,20 +1,21 @@
 import * as S from "./style";
+interface GameItemListProps {
+  children: React.ReactNode;
+}
 
-const GameItemList = () => {
-	return (
-		<section>
-			<S.GameItemListHeader>
-				<S.GameItemListHeaderTitle>
-					Escolha seu jogo
-				</S.GameItemListHeaderTitle>
-				<S.GameItemListHeaderSelect>
-					<option value="default">Selecionar</option>
-					<option value="">xxxxx</option>
-				</S.GameItemListHeaderSelect>
-			</S.GameItemListHeader>
-			<S.GameItemList></S.GameItemList>
-		</section>
-	);
+const GameItemList = ({ children }: GameItemListProps) => {
+  return (
+    <section>
+      <S.GameItemListHeader>
+        <S.GameItemListHeaderTitle>Escolha seu jogo</S.GameItemListHeaderTitle>
+        <S.GameItemListHeaderSelect>
+          <option value="default">Selecionar</option>
+          <option value="">xxxxx</option>
+        </S.GameItemListHeaderSelect>
+      </S.GameItemListHeader>
+      <S.GameItemList>{children}</S.GameItemList>
+    </section>
+  );
 };
 
 export default GameItemList;
