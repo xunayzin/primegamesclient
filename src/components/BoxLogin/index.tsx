3100e40ee74b4fab266a98f02e6413a2e12d81ef
Your Name <mxunay@gmail.com>
2022-09-29 19:05:03 -0300

    feat(BoxLogin): stylization `BoxLogin`

--- src/components/BoxLogin/index.tsx
+++ src/components/BoxLogin/index.tsx
@@ -1,10 +1,21 @@
 import * as S from "./style";
+import logo from "assets/imgs/logo.png";
 
 const BoxLogin = () => {
-	return (
-      <S.BoxLogin>
-      
-      </S.BoxLogin>
+  return (
+    <S.BoxLogin>
+      <S.BoxLoginLogo>
+        <S.BoxLoginLogoText>
+          <span>PRIME</span>
+          <span>GAMES</span>
+        </S.BoxLoginLogoText>
+        <S.BoxLoginLogoImage src={logo} alt="Logo" />
+      </S.BoxLoginLogo>
+      <S.BoxLoginForm>
+        <input type="text" placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+      </S.BoxLoginForm>
+    </S.BoxLogin>
   );
 };
 
