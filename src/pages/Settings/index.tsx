ac485333df491454526feaf72d66ef8c15b54b43
Your Name <mxunay@gmail.com>
2022-10-06 16:05:44 -0300

    doc: inserted `Settings`config

---
+++ src/pages/Settings/index.tsx
@@ -0,0 +1,31 @@
+import Menu from "components/menu";
+import { RoutePath } from "types/routes";
+import { navigationItems } from "data/navigation";
+import * as S from "./style";
+import { Outlet } from "react-router-dom";
+
+const Settings = () => {
+  return (
+    <S.Settings>
+      <Menu active={RoutePath.SETTINGS} navItems={navigationItems} />
+      <S.SettingsPage>
+        <header>
+          <S.SettingsPageHeaderTitle>Configurações</S.SettingsPageHeaderTitle>
+        </header>
+        <S.SettingsContent>
+          <S.SettingsContentSidebar>
+            {"Componente NavColumn"}
+          </S.SettingsContentSidebar>
+          <S.SettingsContentBox>
+            <S.SettingsContentBoxEmpty>
+              Selecione uma categoria
+            </S.SettingsContentBoxEmpty>
+            <Outlet />
+          </S.SettingsContentBox>
+        </S.SettingsContent>
+      </S.SettingsPage>
+    </S.Settings>
+  );
+};
+
+export default Settings;
--- src/router.tsx
+++ src/router.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import { RoutePath } from "types/routes";
 import Home from "pages/Home/index";
 import Login from "pages/Login";
+import Settings from "pages/Settings";
 
 const Router = () => {
 	return (
 		<Routes>
 			<Route path={RoutePath.LOGIN} element={<Login />} />
 			<Route path={RoutePath.HOME} element={<Home />} />
+			<Route path={RoutePath.SETTINGS} element={<Settings />} />
 		</Routes>
 	);
 };
