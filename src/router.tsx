516d6c47995604576ac9683a220eea9e6fdf2e14
Your Name <mxunay@gmail.com>
2022-10-06 17:19:24 -0300

    doc: created `ManageUsers`

--- src/router.tsx
+++ src/router.tsx
@@ -2,19 +2,24 @@ import { Routes, Route } from "react-router-dom";
 import { RoutePath } from "types/routes";
 import Home from "pages/Home/index";
 import Login from "pages/Login";
-import Settings from "pages/Settings";
 import ManageGames from "components/ManageGames";
+import ManageUsers from "components/ManageUsers";
+import Settings from "pages/Settings";
 
 const Router = () => {
-	return (
-		<Routes>
-			<Route path={RoutePath.LOGIN} element={<Login />} />
-			<Route path={RoutePath.HOME} element={<Home />} />
-			<Route path={RoutePath.SETTINGS} element={<Settings />} />
-			<Route path={RoutePath.SETTINGS_GAMES} element={<ManageGames />} />
-
-		</Routes>
-	);
+  return (
+    <Routes>
+      <Route path={RoutePath.LOGIN} element={<Login />} />
+      <Route path={RoutePath.HOME} element={<Home />} />
+      <Route path={RoutePath.SETTINGS} element={<Settings />}>
+        <Route
+          path={RoutePath.SETTINGS_GAMES}
+          element={<ManageGames />}
+        />
+        <Route path={RoutePath.SETTINGS_USERS} element={<ManageUsers />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default Router;
